3d15f0a856f48ae8d55f0a5decc4bb0c30c25e58
edgartn <edgartn@gmail.com>
2015-06-14 13:37:54 -0300

    Finalizando

---
+++ BiografiaApp/BiografiaApp/BiografiaApp.Windows/Cadastrar.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BiografiaApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Cadastrar : Page
+    {
+
+        public Cadastrar()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private void btnVoltar_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void btnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (tbxNome.Text.Length < 5 || tbxNome.Text.Length > 50) 
+            {
+                MessageDialog msgbox = new MessageDialog("O Nome deve conter entre 5 e 50 caracteres!");
+                msgbox.Commands.Add(new UICommand("Close"));
+                msgbox.ShowAsync();
+
+            } else {
+                using (var client = new System.Net.Http.HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost:51706");
+                    var content = new FormUrlEncodedContent(new[] 
+                    {
+                        new KeyValuePair<string, string>("Nome", tbxNome.Text),
+                        new KeyValuePair<string, string>("DataNascimento", Convert.ToString(dtpDataNasc.Date)),
+                        new KeyValuePair<string, string>("Biografia", tbxBiografia.Text)
+                    
+                    });
+                        var result = client.PostAsync("/api/pessoa", content).Result;
+                        string resultContent = result.Content.ReadAsStringAsync().Result;
+                }
+
+                Frame.Navigate(typeof(MainPage));
+            }
+        }
+    }
+}
--- BiografiaApp/BiografiaApp/BiografiaApp.Windows/MainPage.xaml.cs
+++ BiografiaApp/BiografiaApp/BiografiaApp.Windows/MainPage.xaml.cs
@@ -1,13 +1,12 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Data.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -17,7 +16,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.Web.Http;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -28,9 +26,33 @@ namespace BiografiaApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        public class Pessoa
+        {
+            public int Id { get; set; }
+            public String Nome { get; set; }
+            public DateTime DataNascimento { get; set; }
+            public String Biografia { get; set; }
+        }
+
         public MainPage()
         {
             this.InitializeComponent();
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                var result = client.GetAsync("http://localhost:51706/api/pessoa").Result;
+                string resultContent = result.Content.ReadAsStringAsync().Result;
+
+                List<Pessoa> model = null;
+                model = JsonConvert.DeserializeObject<List<Pessoa>>(resultContent);
+
+                foreach (Pessoa aPessoas in model)
+                {
+                    lvPessoas.Items.Add("Nome: " + aPessoas.Nome + "\n" +
+                                        "Data de Nascimento: " + aPessoas.DataNascimento.ToString("dd/MM/yyyy") + "\n" +
+                                        "Biografia: " + aPessoas.Biografia);
+                }
+            }
         }
 
         private void btnConsultar_Click(object sender, RoutedEventArgs e)
@@ -40,29 +62,29 @@ namespace BiografiaApp
             {
                 var result = client.GetAsync("http://localhost:51706/api/pessoa").Result;
                 string resultContent = result.Content.ReadAsStringAsync().Result;
-                tbxBiografia.Text = resultContent;
+
+                List<Pessoa> model = null;
+                model = JsonConvert.DeserializeObject<List<Pessoa>>(resultContent);
+
+                lvPessoas.Items.Clear();
+
+                foreach (Pessoa aPessoas in model)
+                {
+                    lvPessoas.Items.Add("Nome: " + aPessoas.Nome + "\n" +
+                                        "Data de Nascimento: " + aPessoas.DataNascimento.ToString("dd/MM/yyyy") + "\n" +
+                                        "Biografia: " + aPessoas.Biografia);    
+                }                    
 
             }
-            
+
         }
 
         private void btnCadastrar_Click(object sender, RoutedEventArgs e)
         {
-            using (var client = new System.Net.Http.HttpClient())
-            {
-                client.BaseAddress = new Uri("http://localhost:51706");
-                var content = new FormUrlEncodedContent(new[] 
-            {
-                new KeyValuePair<string, string>("Nome", tbxNome.Text),
-                new KeyValuePair<string, string>("DataNascimento", Convert.ToString(dtpDataNasc.Date)),
-                new KeyValuePair<string, string>("Biografia", tbxBiografia.Text)
-                    
-            });
-                var result = client.PostAsync("/api/pessoa", content).Result;
-                string resultContent = result.Content.ReadAsStringAsync().Result;
-                tbxBiografia.Text = resultContent;
-            }
+            Frame.Navigate(typeof(Cadastrar));
+            
         }
+
 
     }
 }
